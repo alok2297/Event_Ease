904946044bd42485f981c775b686576b04ea72ba
Alok kumar <alokkumar2297812@gmail.com>
2023-08-01 12:27:49 +0530

    Initial

---
+++ src/components/Authentication/Header.js
@@ -0,0 +1,76 @@
+import React from "react";
+import './header.css'; // Import the CSS file
+
+const Header = () => {
+  return (
+    <div>
+    <nav className="Main-tag">
+      <div className="navbar-left">
+        <h1 className="tag-line">India's Favourite Event Organizing Platform</h1>
+      </div>
+      <div className="navbar-right">
+        <a className="button" href="#">
+          <img
+            src="https://image.wedmegood.com/resized/20X/images/icons/download_app_new.png"
+            alt="download-app"
+            className="icon"
+          />
+          <span className="link-App-download">Download App</span>
+        </a>
+        <a className="button" href="#">
+          <img
+            src="https://image.wedmegood.com/resized/20X/images/icons/write_a_review_new.png"
+            alt="write-review"
+            className="icon"
+          />
+          <span className="link-App-review">Write a Review</span>
+        </a>
+      </div>
+      </nav>
+      <ul className="flex-container Section-Navbar">
+        <li>
+          <a className="EventEase" href="#">
+            Event Ease
+          </a>
+        </li>
+        <li>
+          <a className="navbar-brand" href="#">
+            Venues
+          </a>
+        </li>
+        <li>
+          <a className="navbar-brand" href="#">
+            Vendors
+          </a>
+        </li>
+        <li>
+          <a className="navbar-brand" href="#">
+            Photos
+          </a>
+        </li>
+        <li>
+          <a className="navbar-brand" href="#">
+            Real Weddings
+          </a>
+        </li>
+        <li>
+          <a className="navbar-brand" href="#">
+            Blog
+          </a>
+        </li>
+        <li>
+          <a className="navbar-brand" href="#">
+            Shop
+          </a>
+        </li>
+        <li>
+          <a className="navbar-brand" href="#">
+            E-Invites
+          </a>
+        </li>
+      </ul>
+      </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Authentication/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import Login from "./Login";
+
+
+function LoginPage(){
+    return (<div>
+        <Header />
+        <Login />
+    </div>)
+}
+
+export default LoginPage;
