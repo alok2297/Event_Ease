f04590ffb696c90735f7e48ac571411bb118f2f0
Alok kumar <alokkumar2297812@gmail.com>
2023-10-06 13:03:03 +0530

    popup completed

--- src/App.js
+++ src/App.js
@@ -2,29 +2,11 @@ import "./header.css";
 import "./components/Header";
 import Header from "./components/Header";
 import HeroSection from "./components/MainActivity/HeroSection";
-import CitiesPopup from "./components/MainActivity/CitiesPopup";
-import { useState } from "react";
-import Modal from "@mui/material/Modal";
-import { set } from "mongoose";
 
 function App() {
-  const [open, setOpen] = useState(false);
-  const handleClose = () => setOpen(false);
-  const handlePop = (e) => {
-    console.log("Vaibhav");
-    setOpen(e);
-  };
   return (
     <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <CitiesPopup />
-      </Modal>
-      <Header OpenCityPop={handlePop} />
+      <Header  />
       <HeroSection />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import CitiesPopup from "./MainActivity/CitiesPopup";
+import Modal from "@mui/material/Modal";
 
-const Header = (props) => {
+const Header = () => {
+  const Vendors = [
+    "Venues",
+    "Vendors",
+    "Photos",
+    "Real Wedding",
+    "Blog",
+    "Shop",
+  ];
 
-  const Vendors=["Venues","Vendors","Photos","Real Wedding","Blog","Shop"];
+  const [open, setOpen] = useState(false);
 
-   const handleClick=()=>{
-    console.log(true); 
-    props.OpenCityPop(true);
-  }
+  const handleClick = () => {
+    console.log(true);
+    // props.OpenCityPop(true);
+    setOpen(true); // Open the modal
+  };
 
   return (
     <div>
-    <nav className="Main-tag">
-      <div className="navbar-left">
-        <h1 className="tag-line">India's Favourite Event Organizing Platform</h1>
-      </div>
-      <dniv className="header-cities" onClick={handleClick} style={{height:"30px", width:"200px",backgroundColor:"white" }}>
-        <div className="wedding-city" style={{marginLeft:"10px", color:"black", width:"auto", height:"auto"}}>Udaipur</div>
-        <i className="fa fa-caret-down h5 text-secondary v-center" style={{color:"black", alignItems:"center", alignSelf:"center", marginLeft:"auto", marginRight:"10px"}}></i>
-      </dniv>
-      <div className="navbar-right">
-        <a className="button-header" href="#">
-          <img
-            src="https://image.wedmegood.com/resized/20X/images/icons/download_app_new.png"
-            alt="download-app"
-            className="icon"
-          />
-          <span className="link-App-download">Download App</span>
-        </a>
-        <a className="button-header" href="#">
-          <img
-            src="https://image.wedmegood.com/resized/20X/images/icons/write_a_review_new.png"
-            alt="write-review"
-            className="icon"
-          />
-          <span className="link-App-review">Write a Review</span>
-        </a>
-      </div>
+      <nav className="Main-tag">
+        <div className="navbar-left">
+          <h1 className="tag-line">
+            India's Favorite Event Organizing Platform
+          </h1>
+        </div>
+        <div
+          className="header-cities"
+          onClick={handleClick}
+          style={{ height: "30px", width: "200px", backgroundColor: "white" }}
+        >
+          <div
+            className="wedding-city"
+            style={{
+              marginLeft: "10px",
+              color: "black",
+              width: "auto",
+              height: "auto",
+            }}
+          >
+            Udaipur
+          </div>
+          <i
+            className="fa fa-caret-down h5 text-secondary v-center"
+            style={{
+              color: "black",
+              alignItems: "center",
+              alignSelf: "center",
+              marginLeft: "auto",
+              marginRight: "10px",
+            }}
+          ></i>
+        </div>
+        <div className="navbar-right">
+          <a className="button-header" href="#">
+            <img
+              src="https://image.wedmegood.com/resized/20X/images/icons/download_app_new.png"
+              alt="download-app"
+              className="icon"
+            />
+            <span className="link-App-download">Download App</span>
+          </a>
+          <a className="button-header" href="#">
+            <img
+              src="https://image.wedmegood.com/resized/20X/images/icons/write_a_review_new.png"
+              alt="write-review"
+              className="icon"
+            />
+            <span className="link-App-review">Write a Review</span>
+          </a>
+        </div>
+        <Modal
+          style={{
+            width: "0%",
+            marginLeft: " auto",
+            marginRight: "auto",
+            backgroundColor: "transparent",
+          }}
+          open={open}
+          onClose={() => setOpen(false)}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <CitiesPopup />
+        </Modal>
       </nav>
       <ul className="flex-container Section-Navbar">
         <li>
@@ -45,20 +95,24 @@ const Header = (props) => {
           </a>
         </li>
         {Vendors.map((item, index) => (
-        <li key={index}>
-          <a className="navbar-brand" name={item}>
-            {item}
-          </a>
-        </li>
-      ))}
+          <li key={index}>
+            <a className="navbar-brand" name={item}>
+              {item}
+            </a>
+          </li>
+        ))}
         <li>
           <a className="navbar-brand" href="#">
             E-Invites
-            <img src="https://image.wedmegood.com/resized/30X/images/deals/store-new-badges.png" style={{marginLeft:5}} alt=""/>
+            <img
+              src="https://image.wedmegood.com/resized/30X/images/deals/store-new-badges.png"
+              style={{ marginLeft: 5 }}
+              alt=""
+            />
           </a>
         </li>
       </ul>
-      </div>
+    </div>
   );
 };
 
