9bc886eb109e77657cb0bf7c6b9258e9ed58aa23
Sarfaraj Ansari <sarfarajansari2000@gmail.com>
2023-12-26 00:02:36 +0530

    fix: slider fixes

--- src/components/SliderTwo/SliderTwo.js
+++ src/components/SliderTwo/SliderTwo.js
@@ -4,7 +4,7 @@ const SliderTwo = ()=>{
    <section class="slider_container middle-container">
        <div class="container ">
            <div class="swiper popular_slider">
-               <div class="swiper-wrapper middle-wrppaer">
+               <div class="swiper-wrapper middle-wrapper">
                    <div class="swiper-slide each-slide">
                        <div class="popular-search-image">
                            <div class="container-img ">
@@ -42,6 +42,39 @@ const SliderTwo = ()=>{
                        <div class="popular-search-image">
                            <div class="container-img">
                                <a href="$"> 
+                                   <img class="popsearch_img" alt=""
+                                       src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"/>
+                               </a>
+                           </div>
+                           <p>Bridal Wear in Lucknow</p>
+                       </div>
+                   </div>
+                   <div class="swiper-slide each-slide">
+                       <div class="popular-search-image">
+                           <div class="container-img">
+                               <a href="$">
+                                   <img class="popsearch_img" alt=""
+                                       src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"/>
+                               </a>
+                           </div>
+                           <p>Bridal Wear in Lucknow</p>
+                       </div>
+                   </div>
+                   <div class="swiper-slide each-slide">
+                       <div class="popular-search-image">
+                           <div class="container-img">
+                               <a href="$">
+                                   <img class="popsearch_img" alt=""
+                                       src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"/>
+                               </a>
+                           </div>
+                           <p>Bridal Wear in Lucknow</p>
+                       </div>
+                   </div>
+                   <div class="swiper-slide each-slide">
+                       <div class="popular-search-image">
+                           <div class="container-img">
+                               <a href="$">
                                    <img class="popsearch_img" alt=""
                                        src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"/>
                                </a>
