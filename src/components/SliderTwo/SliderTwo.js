db22aec5576dce34a3c68cc9efb222132f36e4a7
Sarfaraj Ansari <sarfarajansari2000@gmail.com>
2024-01-04 19:18:59 +0530

    fix: slider bugs

--- src/components/SliderTwo/SliderTwo.js
+++ src/components/SliderTwo/SliderTwo.js
@@ -20,131 +20,139 @@ const SliderTwo = ()=>{
   const nextHandler = () => {
     swiperRef.slideNext();
   };
+  const breakPoints = {
+    default: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 480px
+    sm: {
+      slidesPerView: 4,
+    },
+    md: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 640px
+    lg: {
+      slidesPerView: 8,
+    },
+  };
    return <Container children={
-    <div>
-        <div class="real-wedding">Popular Searchers</div>
-        <Swiper
-            // install Swiper modules
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={100}
-            slidesPerView={5}
-            speed={2000}
-            onSwiper={(swiper) => setSwiperRef(swiper)}
-          >
-            <SwiperSlide>
-             <div className="wrapper-slider">
-                <div>
-                  <div className="wrapper-image">
-                    <img
-                      src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="weding-text">Vedika and Rishi</div>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-             <div className="wrapper-slider">
-                <div>
-                  <div className="wrapper-image">
-                    <img
-                      src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="weding-text">Vedika and Rishi</div>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-             <div className="wrapper-slider">
-                <div>
-                  <div className="wrapper-image">
-                    <img
-                      src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="weding-text">Vedika and Rishi</div>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-             <div className="wrapper-slider">
-                <div>
-                  <div className="wrapper-image">
-                    <img
-                      src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="weding-text">Vedika and Rishi</div>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-             <div className="wrapper-slider">
-                <div>
-                  <div className="wrapper-image">
-                    <img
-                      src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="weding-text">Vedika and Rishi</div>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-             <div className="wrapper-slider">
-                <div>
-                  <div className="wrapper-image">
-                    <img
-                      src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="weding-text">Vedika and Rishi</div>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-             <div className="wrapper-slider">
-                <div>
-                  <div className="wrapper-image">
-                    <img
-                      src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="weding-text">Vedika and Rishi</div>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-             <div className="wrapper-slider">
-                <div>
-                  <div className="wrapper-image">
-                    <img
-                      src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="weding-text">Vedika and Rishi</div>
-                </div>
-              </div>
-            </SwiperSlide>
-          </Swiper>
-          <div className="btn-container">
-            <button className="prev" onClick={prevHandler}>
-              <Iconify width={40} height={40} color={"#4a4a4a"} icon="mdi:chevron-left"></Iconify>
-            </button>
-            <button className="next" onClick={nextHandler}>
-              <Iconify width={40} height={40} color={"#4a4a4a"} icon="mdi:chevron-right"></Iconify>
-            </button>
-          </div>
-          </div>
+        <><div class="real-wedding">Popular Searchers</div><div className="slider-container">
+       <Swiper
+         // install Swiper modules
+         modules={[Navigation, Pagination, Scrollbar, A11y]}
+         spaceBetween={10}
+         slidesPerView={5}
+         speed={2000}
+         onSwiper={(swiper) => setSwiperRef(swiper)}
+         breakpoints={breakPoints}
+       >
+         <SwiperSlide>
+           <div className="wrapper-slider">
+             <div>
+               <div className="wrapper-image">
+                 <img
+                   src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
+                   alt="" />
+               </div>
+               <div className="weding-text">Vedika and Rishi</div>
+             </div>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+           <div className="wrapper-slider">
+             <div>
+               <div className="wrapper-image">
+                 <img
+                   src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
+                   alt="" />
+               </div>
+               <div className="weding-text">Vedika and Rishi</div>
+             </div>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+           <div className="wrapper-slider">
+             <div>
+               <div className="wrapper-image">
+                 <img
+                   src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
+                   alt="" />
+               </div>
+               <div className="weding-text">Vedika and Rishi</div>
+             </div>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+           <div className="wrapper-slider">
+             <div>
+               <div className="wrapper-image">
+                 <img
+                   src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
+                   alt="" />
+               </div>
+               <div className="weding-text">Vedika and Rishi</div>
+             </div>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+           <div className="wrapper-slider">
+             <div>
+               <div className="wrapper-image">
+                 <img
+                   src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
+                   alt="" />
+               </div>
+               <div className="weding-text">Vedika and Rishi</div>
+             </div>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+           <div className="wrapper-slider">
+             <div>
+               <div className="wrapper-image">
+                 <img
+                   src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
+                   alt="" />
+               </div>
+               <div className="weding-text">Vedika and Rishi</div>
+             </div>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+           <div className="wrapper-slider">
+             <div>
+               <div className="wrapper-image">
+                 <img
+                   src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
+                   alt="" />
+               </div>
+               <div className="weding-text">Vedika and Rishi</div>
+             </div>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+           <div className="wrapper-slider">
+             <div>
+               <div className="wrapper-image">
+                 <img
+                   src="https://image.wedmegood.com/resized/300X/uploads/banner_image/1/bridal-wear.jpg"
+                   alt="" />
+               </div>
+               <div className="weding-text">Vedika and Rishi</div>
+             </div>
+           </div>
+         </SwiperSlide>
+       </Swiper>
+       <div className="btn-container">
+         <button className="prev" onClick={prevHandler}>
+           <Iconify width={40} height={40} color={"#4a4a4a"} icon="mdi:chevron-left"></Iconify>
+         </button>
+         <button className="next" onClick={nextHandler}>
+           <Iconify width={40} height={40} color={"#4a4a4a"} icon="mdi:chevron-right"></Iconify>
+         </button>
+       </div>
+     </div></>
    }></Container>
 }
 export default SliderTwo;
