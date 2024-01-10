2cb2e0a8c94c28c4c5ae479bb681de5ae0cacb56
Sarfaraj Ansari <sarfarajansari2000@gmail.com>
2024-01-10 15:14:53 +0530

    fix: category and breadcrumbs refactored

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,20 +6,21 @@ import slides from "../LocalFile/PopularVenues.json"
 import Category from "../components/Blocks/Category/Category";
 import SliderTwo from "../components/Blocks/SliderTwo/SliderTwo";
 import WMGInHouse from "../components/Blocks/WMG/WMGInHouse";
+import Categories from "../LocalFile/WeddingCategory.json";
 
 const HomePage = (props) => {
   return (
     <div>
-      <HeroSection selectedCity={props.city}/>
-      <PopularSlider slides={slides} swiperHeight="150px" boxImgWidth="180px" boxImgHeight="120px"/>
-      <SliderTwo/>
-      <Category/>
-      <WMGInHouse/>
-      <NewsLetter/>
-      <WeddingBlogSlider slideView = "3" wedWidth = "390px"/>
-      <SliderTwo/>
-      <WeddingBlogSlider slideView = "4" wedWidth = "344px"/>
-      <WeddingBlogSlider  slideView = "3" wedWidth = "390px"/>
+      <HeroSection selectedCity={props.city} />
+      <PopularSlider slides={slides} swiperHeight="150px" boxImgWidth="180px" boxImgHeight="120px" />
+      <SliderTwo />
+      <Category show={false} category={Categories.slice(0, 6)} />
+      <WMGInHouse />
+      <NewsLetter />
+      <WeddingBlogSlider slideView="3" wedWidth="390px" />
+      <SliderTwo />
+      <WeddingBlogSlider slideView="4" wedWidth="344px" />
+      <WeddingBlogSlider slideView="3" wedWidth="390px" />
     </div>
   )
 }
--- src/components/Blocks/Category/Category.js
+++ src/components/Blocks/Category/Category.js
@@ -1,10 +1,9 @@
 import './Category.css';
 import { Container } from "../../Elements/Container"
 import { Iconify } from "../../Elements/Icon";
-import WeddingCategory from '../../../LocalFile/WeddingCategory.json';
 import { Breadcrumbs } from "../../Elements/Breadcrumbs";
 
-const Category = () => {
+const Category = (props) => {
     const links = [
         {
             path: "/",
@@ -18,7 +17,7 @@ const Category = () => {
     return (
         <Container children={
             <div className="">
-                <Breadcrumbs links={links}></Breadcrumbs>
+                {props.show && <Breadcrumbs links={links}></Breadcrumbs>}
                 <div className="heading">
                     <div className="Wedding-Categories">Wedding Categories</div>
                     <div className="All-Categories">View all Categories
@@ -26,20 +25,20 @@ const Category = () => {
                     </div>
                 </div>
                 <div className="Categories">
-                    {WeddingCategory.slice(8).map((item)=>(
-                        <div className="First_Category Common_Category" key={item.id} style={{backgroundColor:item.color}}>
-                        <div className="text_div">
-                            <div className="text_div_container">
-                                <div className="text_div_first">{item.title}</div>
-                                <div className="text_div_second">{item.subtitle}</div>
+                    {props.category.map((item, index) => (
+                        <div className="First_Category Common_Category" key={index + props.id} style={{ backgroundColor: item.color }}>
+                            <div className="text_div">
+                                <div className="text_div_container">
+                                    <div className="text_div_first">{item.title}</div>
+                                    <div className="text_div_second">{item.subtitle}</div>
+                                </div>
+                            </div>
+                            <div className="image_div">
+                                <img className="first_image"
+                                    src={item.imgUrl}
+                                    alt="categoryImg" />
                             </div>
                         </div>
-                        <div className="image_div">
-                            <img className="first_image"
-                                src={item.imgUrl}
-                                alt="categoryImg" />
-                        </div>
-                    </div>
                     ))}
                 </div>
             </div>
