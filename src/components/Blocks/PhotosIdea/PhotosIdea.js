1bfc79a97c454d5f5e478c08a76a82d63ae29ef1
Sarfaraj Ansari <sarfarajansari2000@gmail.com>
2024-02-17 16:55:24 +0530

    fix: removed redundant code

--- src/components/Blocks/PhotosIdea/PhotosIdea.js
+++ src/components/Blocks/PhotosIdea/PhotosIdea.js
@@ -2,53 +2,30 @@ import React from 'react'
 import Datas from "../../../Data/PhotoIdeas.json"
 import "./PhotosIdea.css"
 const PhotosIdea = () => {
+
+  const dynamicGrid =(items) => {
+    return(
+      <div className='photos-wrapper'>
+      {items.map((data, i) => (
+        <div className='photos-container' key={i}>
+          <div style={{ height: data.height }}>
+            <img src={data.image} alt={data.title} />
+          </div>
+          <h2 className='title'>{data.title}</h2>
+          <span className='location'>{data.location}</span>
+          <p className='description'>{data.description}</p>
+        </div>
+      ))}
+    </div>
+    )
+
+  }
   return (
     <div className='main-container'>
-      <div className='photos-wrapper'>
-        {Datas.slice(0,7).map((data, i) => (
-          <div className='photos-container' key={i}>
-            <div style={{ height: data.height }}>
-              <img src={data.image} alt={data.title} />
-            </div>
-            <h2 className='title'>{data.title}</h2>
-            <span className='location'>{data.location}</span>
-            <p className='description'>{data.description}</p>
-          </div>
-        ))}
-      </div><div className='photos-wrapper'>
-        {Datas.slice(2,7).map((data, i) => (
-          <div className='photos-container' key={i}>
-            <div style={{ height: data.height }}>
-              <img src={data.image} alt={data.title} />
-            </div>
-            <h2 className='title'>{data.title}</h2>
-            <span className='location'>{data.location}</span>
-            <p className='description'>{data.description}</p>
-          </div>
-        ))}
-      </div><div className='photos-wrapper'>
-        {Datas.slice(1,8).map((data, i) => (
-          <div className='photos-container' key={i}>
-            <div style={{ height: data.height }}>
-              <img src={data.image} alt={data.title} />
-            </div>
-            <h2 className='title'>{data.title}</h2>
-            <span className='location'>{data.location}</span>
-            <p className='description'>{data.description}</p>
-          </div>
-        ))}
-      </div><div className='photos-wrapper'>
-        {Datas.slice(3,9).map((data, i) => (
-          <div className='photos-container' key={i}>
-            <div style={{ height: data.height }}>
-              <img src={data.image} alt={data.title} />
-            </div>
-            <h2 className='title'>{data.title}</h2>
-            <span className='location'>{data.location}</span>
-            <p className='description'>{data.description}</p>
-          </div>
-        ))}
-      </div>
+      {dynamicGrid(Datas.slice(0,7))}
+      {dynamicGrid(Datas.slice(2,9))}
+      {dynamicGrid(Datas.slice(1,8))}
+      {dynamicGrid(Datas.slice(3,10))}
     </div>
   )
 }
