211ac46ee1acbe1c5ee41db8bc3e318c2a1c6699
Sarfaraj Ansari <sarfarajansari2000@gmail.com>
2024-05-29 16:50:01 +0530

    router error handling added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import "./header.css";
 import "./components/Blocks/Header/Header";
-import { PageLoader } from "./Pages/PageLoader";
-import {getRoutes} from "./Theme/Theme"
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import createRouter from "Router";
 
 function App() {
-  const routes = Object.values(getRoutes() || {}).map(route => ({
-    ...route,
-    element: <PageLoader/>,
-  }));
-
-  const router = createBrowserRouter([
-    ...routes,
-  ])
+  const router = createRouter()
   return (
     <div>
       <RouterProvider router={router}/>
--- src/Router/index.js
+++ src/Router/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import {PageLoader} from '../Pages/PageLoader';
 import {getRoutes} from "../Theme/Theme"
+import PageNotFound from 'components/Blocks/PageNotFound/PageNotFound';
 
 // Function to create the router
 const createRouter = () => {
   const routes = Object.values(getRoutes() || {}).map(route => ({
     ...route,
     element: <PageLoader/>,
+    errorElement: <PageNotFound/>,
   }));
 
   const router = createBrowserRouter([
